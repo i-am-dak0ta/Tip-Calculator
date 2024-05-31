235a29138374c8f5cf60031bef81d8d65fc94110
Artur Kuznetsov <arturkuz.dkt@gmail.com>
2024-05-31 20:09:44 +0300

    feat: added functionality for calculating tips in MainActivity

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val percentage = when (selectedId) {
+            R.id.option_five_percent -> 0.05
+            R.id.option_ten_percent -> 0.1
+            else -> 0.15
+        }
+        var tip = cost * percentage
+        val round = binding.roundSwitch.isChecked
+        if (round) {
+            tip = ceil(tip)
+        }
+
+        val currencyTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, currencyTip)
     }
 }
